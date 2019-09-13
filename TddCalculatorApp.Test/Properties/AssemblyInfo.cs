042b292b8a0c5ecb391fe76abc7474ed565f90c4
gagannn <gagany400@gmail.com>
2019-09-13 10:26:46 +0530

    Initial Failed Tests

---
+++ TddCalculatorApp.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TddCalculatorApp.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TddCalculatorApp.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b5bc7d4-7169-47c2-aa47-87f0e0dc7b7c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TddCalculatorApp.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddCalculatorApp.Library;
+
+namespace TddCalculatorApp.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldReturnPositiveOnPassingTwoPositiveNumbers()
+        {
+            Calculator calObj = new Calculator();
+            int num1 = 3;
+            int num2 = 4;
+            int result;
+            result = calObj.AddIntegers(num1, num2);
+
+            Assert.AreEqual(7, result, "Testing two integers 3 and 2");
+        }
+
+       
+    }
+}
